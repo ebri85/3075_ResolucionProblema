641b193d3399cd033987a44c2239ee4c1d9f4033
Esau Brizuela <ebri_85@outlook.com>
2020-10-18 16:01:02 -0600

    Se cargan los servicios predeterminados

--- scripts/script.js
+++ scripts/script.js
@@ -1,16 +1,9 @@
 
 const myHtml = window;
 
-
 myHtml.onload = function(){
 
-   cargaPredeterminados();
-
-
-}
-
-function cargaPredeterminados(){
-
+    
     const Pedicure = {
         codigo: '2020-ped-p01',
         descripcion: 'Servicio de Pedicure y rehabilitacion de tu pie',
@@ -40,5 +33,44 @@ function cargaPredeterminados(){
         combo: true,
         imagen: 'img/mantenimiento.png' 
     }
+    
+const servicio1 = document.querySelector(".servicio-1"); 
+const s1 = servicio1.parentNode;
+
+    s1.querySelector(".img").src="img/pedicure.png"
+    s1.querySelector(".codigo").innerHTML= Pedicure.codigo;
+    s1.querySelector(".descripcion").innerHTML= Pedicure.descripcion; 
+    s1.querySelector(".acabado").innerHTML= Pedicure.acabado; 
+    s1.querySelector(".multicolor").innerHTML= Pedicure.multicolor; 
+    s1.querySelector(".cmbo").innerHTML= Pedicure.combo; 
+
+
+
+    const servicio2 = document.querySelector(".servicio-2"); 
+    const s2 = servicio2.parentNode;
+    
+        s2.querySelector(".img").src="img/manicure.png"
+        s2.querySelector(".codigo").innerHTML= Manicure.codigo;
+        s2.querySelector(".descripcion").innerHTML= Manicure.descripcion; 
+        s2.querySelector(".acabado").innerHTML= Manicure.acabado; 
+        s2.querySelector(".multicolor").innerHTML= Manicure.multicolor; 
+        s2.querySelector(".cmbo").innerHTML= Manicure.combo;     
+
+        const servicio3 = document.querySelector(".servicio-3"); 
+        const s3 = servicio3.parentNode;
+        
+        s3.querySelector(".img").src="img/mantenimiento.png"
+        s3.querySelector(".codigo").innerHTML= Mantenimiento.codigo;
+        s3.querySelector(".descripcion").innerHTML= Mantenimiento.descripcion; 
+        s3.querySelector(".acabado").innerHTML= Mantenimiento.acabado; 
+        s3.querySelector(".multicolor").innerHTML= Mantenimiento.multicolor; 
+        s3.querySelector(".cmbo").innerHTML= Mantenimiento.combo;     
+    
+    
 
 }
+
+
+
+
+
