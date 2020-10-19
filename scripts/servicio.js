502e28201ba19054564c2af5605f4d4e66f24fe5
Esau Brizuela <ebri_85@outlook.com>
2020-10-18 20:11:22 -0600

    Se deja listo la validacion de el codigo de servicio y se logra comprender subir al localstorage

--- scripts/servicio.js
+++ scripts/servicio.js
@@ -3,14 +3,25 @@ const myhtml = window;
 
 
 myhtml.onload = function(){
+    class Servicio  {
+        constructor(codigo,descripcion,costo,acabado,multicolor,combo,imagen){
+            this.codigo=codigo,
+            this.descripcion = descripcion,
+            this.costo = costo,
+            this.acabado= acabado,
+            this.multicolor = multicolor,
+            this.combo = combo,
+            this.imagen = imagen
+        }
+    }
 
 
+    const codigo = document.querySelector("#codigo");
+    const boton = document.querySelector('button');
 
-    let codigo = document.querySelector("#codigo");
-
-    codigo.addEventListener('input',function(e){
-        alert("Escribiendo");
-    });
+      
+  
+        
     let descripcion = document.querySelector("#descripcion");
     let costo = document.querySelector("#costo");
     let acabado = document.querySelector("#acabados");
@@ -20,20 +31,45 @@ myhtml.onload = function(){
     let comboNo = document.querySelector("#comboNo");
     let imagen = document.querySelector("#imagen");
   
-console.log(codigo);
-console.log(descripcion);
-console.log(costo);
-console.log(acabado);
-console.log(multicolorSi);
-console.log(multicolorNo);
-console.log(comboSi);
-console.log(comboNo);
-console.log(imagen);
 
 
+    boton.addEventListener('click', function(e) {
+
+             
+        const pat = /[0-9]{4}-[a-zA-Z]{3}-[a-zA-Z]{1}[0-9]{3}/g;
+
+        let str = codigo.value;
+            
+        let res = pat.test(str);
+        if(!res){
+            alert("Por favor corriga el formato del codigo del servicio");
+        }
+
+    
+        let servicio = new Servicio(
+            codigo.value,
+            descripcion.value,
+            costo.value,
+            acabado.value,
+            multicolor = function() {
+                if(multicolorSi===true){
+                    return true;
+                }else{
+                    return false;
+                }
+            },
+            combo =function(){
+                if(comboSi.value===true){
+                    return true;
+                }else{
+                    return false;
+                }
+            },        
+            imagen.value)
+        
+        localStorage.setItem(servicio, JSON.stringify(servicio));
+    });
+   
 
 }
 
-const input = document.querySelector('input');
-
-input.addEventListener('input',)
